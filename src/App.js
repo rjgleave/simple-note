9142d6fa302540bd3dd8296bb816777b5c0badd0
“Rob <rjgleave@amazon.com>
2020-05-23 09:49:06 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,175 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Amplify, {API,graphqlOperation} from 'aws-amplify';
+import { withAuthenticator} from 'aws-amplify-react'; 
+import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
+Amplify.configure(aws_exports);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const createNote = `mutation createNote($note: String!){
+  createNote(input:{
+    note: $note
+  }){
+    __typename
+    id
+    note
+    description
+  }
+}`;
+
+const readNote = `query listNotes{
+  listNotes{
+    items{
+      __typename
+      id
+      note
+      description
+    }
+  }
+}`;
+
+const updateNote = `mutation updateNote($id: ID!,$note: String){
+  updateNote(input:{
+    id: $id
+    note: $note
+  }){
+    __typename
+    id
+    note
+    description
+  }
+}`;
+
+const deleteNote = `mutation deleteNote($id: ID!){
+  deleteNote(input:{
+    id: $id
+  }){
+    __typename
+    id
+    note
+  }
+}`;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      id:"",
+      notes:[],
+      value:"",
+      displayAdd:true,
+      displayUpdate:false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  async componentDidMount(){
+    const notes = await API.graphql(graphqlOperation(readNote));
+    this.setState({notes:notes.data.listNotes.items});
+  }
+
+  handleChange(event) {
+    this.setState({value:event.target.value});
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const note = {"note":this.state.value, "description": description.value}
+    await API.graphql(graphqlOperation(createNote, note));
+    this.listNotes();
+    this.setState({value:""});
+  }
+  async handleDelete(id) {
+    const noteId = {"id":id};
+    await API.graphql(graphqlOperation(deleteNote, noteId));
+    this.listNotes();
+  }
+  async handleUpdate(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const note = {"id":this.state.id,"note":this.state.value};
+    await API.graphql(graphqlOperation(updateNote, note));
+    this.listNotes();
+    this.setState({displayAdd:true,displayUpdate:false,value:""});
+  }
+  selectNote(note){
+    this.setState({id:note.id,value:note.note,displayAdd:false,displayUpdate:true});
+  }
+  async listNotes(){
+    const notes = await API.graphql(graphqlOperation(readNote));
+    this.setState({notes:notes.data.listNotes.items});
+  }
+  
+  render() {
+    const data = [].concat(this.state.notes)
+      .map((item,i)=> 
+      <div className="alert alert-primary alert-dismissible show" role="alert">
+        <span key={item.i} onClick={this.selectNote.bind(this, item)}>{item.note}</span>
+        <button key={item.i} type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={this.handleDelete.bind(this, item.id)}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      )
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">SA Request Form</h1>
+        </header>
+        <br/>
+        <div className="container">
+          {this.state.displayAdd ?
+            <form onSubmit={this.handleSubmit}>
+
+              <div className="input-group mb-3">
+                <input type="text" className="form-control form-control-lg" placeholder="Title" aria-label="Note" aria-describedby="basic-addon2" value={this.state.value} onChange={this.handleChange}/>
+              </div>
+              <div class="input-group mb-3">
+                <select class="custom-select" id="inputGroupSelect01">
+                  <option selected>Activity Type</option>
+                  <option value="Meeting">Meeting</option>
+                  <option value="Immersion Day">Immersion Day</option>
+                  <option value="Architecture Review">Architecture Review</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <textarea class="form-control"  placeholder="Description"></textarea>
+              </div>
+              <button className="btn btn-primary" type="submit">Submit</button>
+            </form>
+          : null }
+          {this.state.displayUpdate ?
+            <form onSubmit={this.handleUpdate}>
+
+              <div className="input-group mb-3">
+                <input type="text" className="form-control form-control-lg" placeholder="Title" aria-label="Note" aria-describedby="basic-addon2" value={this.state.value} onChange={this.handleChange}/>
+              </div>
+              <div class="input-group mb-3">
+                <select class="custom-select" id="inputGroupSelect01">
+                  <option selected>Activity Type</option>
+                  <option value="Meeting">Meeting</option>
+                  <option value="Immersion Day">Immersion Day</option>
+                  <option value="Architecture Review">Architecture Review</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <textarea class="form-control"  placeholder="Description" id="description"></textarea>
+              </div>
+
+
+              <button className="btn btn-primary" type="submit">Update Note</button>
+            </form>
+          : null }
+        </div>
+        <br/>
+        <div className="container">
+          {data}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
